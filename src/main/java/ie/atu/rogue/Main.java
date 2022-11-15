0b87031acdbc156a1ba3cdb437efa269e997d7c1
Dylan Walsh <L00163199@atu.ie>
2022-11-15 19:18:24 +0000

    Testing file modification

--- src/main/java/ie/atu/rogue/Main.java
+++ src/main/java/ie/atu/rogue/Main.java
@@ -2,6 +2,7 @@ package ie.atu.rogue;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Rogue22!");
+        System.out.println("Test2");
     }
 }
