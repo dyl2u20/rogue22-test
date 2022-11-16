e9f72bd6e17149532d41fc1de87f9c9047b48bdb
Dylan Walsh <L00163199@atu.ie>
2022-11-16 03:25:18 +0000

    Added simple example of hard-coded password to test SAST compliance

--- src/main/java/ie/atu/rogue/Main.java
+++ src/main/java/ie/atu/rogue/Main.java
@@ -1,8 +1,15 @@
 package ie.atu.rogue;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Rogue22!");
-        System.out.println("Test2");
+    public static void main(String[] args) throws SQLException {
+        // Hard-coded password to check SAST compliance
+        String username = "dylan";
+        String password = "walsh";
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+                "user=" + username + "&password=" + password); // Sensitive
     }
 }
